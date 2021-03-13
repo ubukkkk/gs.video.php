f7d8aac411906fddbc22f779295bb22dd543f950
ubukkkk <80142170+ubukkkk@users.noreply.github.com>
2021-03-13 16:04:38 +0900

    Add files via upload

---
+++ test01.php
@@ -0,0 +1,16 @@
+<?php
+    echo "aaaa<br>";
+    echo "bb<br>bb<br>";
+    echo "cccc<br>";
+
+    $name ="aiueo";
+    $num = 100;
+    $a = 1;
+    $b = 2;
+    $c = 3;
+    echo $name;
+    echo $num;
+    echo $a;
+    echo $b;
+    echo $c;
+?>

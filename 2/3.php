8ca4302bb374e4d28ab503c2356695fcc5aae93a
ubukkkk <80142170+ubukkkk@users.noreply.github.com>
2021-03-19 18:50:39 +0900

    Add files via upload

---
+++ 2/3.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>じゃんけんゲーム(php版)</title>
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+<!-- タイトル -->
+    <br>
+    <div id="title">キュアピースとじゃんけん勝負！</div>
+    <div id="t2">(ボタンをクリックするとじゃんけんできるよ)</div>
+    <br>
+
+<!-- 最初の画像 -->
+<div id="top">
+  <img id ="r9" src="img/r9.jpg">
+</div>
+<br>
+
+<!-- じゃんけんボタン -->
+<form action ="kekka.php" method ="post">
+<div id="bt">
+    <p id="mae">おなまえ:<input type="text" name="name" size="20"></p><br>
+    <button id="b1" value=1 button type ="submit" name ="te">グー</button>
+    <button id="b2" value=2　button type ="submit" name ="te">チョキ</button>
+    <button id="b3" value=3　button type ="submit" name ="te">パー</button>
+</div>
+</form>
+
+</body>
+</html>
